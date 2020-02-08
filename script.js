0cfa6911284dab73f2d8f8fccba008644358981d
Colin Varney <cvcolinv@gmail.com>
2020-02-08 16:06:13 -0500

    Added functionality with event listeners. Project now successfully clears and resizes the grid along with drawing over cells.

--- script.js
+++ script.js
@@ -1,63 +1,59 @@
-//from SO: 
-
-//const container = document.getElementById("container");
-//
-//function makeRows(rows, cols) {
-//  container.style.setProperty('--grid-rows', rows);
-//  container.style.setProperty('--grid-cols', cols);
-//  for (c = 0; c < (rows * cols); c++) {
-//    let cell = document.createElement("div");
-//    cell.innerText = (c + 1);
-//    container.appendChild(cell).className = "grid-item";
-//  };
-//};
-//
-//makeRows(16, 16);
 
 
 
-//grid function that takes value from user input, and creates a grid.
-//non functional. 
-
-let v = prompt("How many rows ya want!?");
 // definition section: 
-let container = document.querySelector('#container');
+let container = document.querySelector('.container');
+let divAdded = document.createElement('div');
 let clearResize = document.querySelector('#clearResize');
+let targetDiv = document.getElementsByClassName('newDiv'); 
+                          
+
+//main prompt
+let v = prompt("How many rows ya want!?");
+
+
+//initial function call
 
 grid(v);
 
 function grid(v) {
   
-  
-   var area = v * v;
+    let area = v * v;
 
     //Sets styles and size for big container
     container.style.gridTemplateRows = `repeat(${v}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${v}, 1fr)`;
   
-  
-    for(let i = 0;  i < area; i++){
-            let divAdded = document.createElement("div");
-            divAdded.className = 'newDiv';
-            container.appendChild(divAdded);
-            console.log("divadded")
-    }
-
     clearResize.addEventListener('click', function() {
         reset();
+        console.log("Cleared")
     });
+  
+  
+    for(let i = 0;  i < area; i++){
+        let divAdded = document.createElement('div');
+        divAdded.className = "newDiv";
+        divAdded.addEventListener("mouseover", function() {
+          this.style.backgroundColor = "black";
+          console.log("Cell Drawn");
+        });
+        container.appendChild(divAdded);
+        console.log("divadded");
+    }
+    
 
 }
 
 
 //resize function
 function reset() {
-    let divsDelete = document.getElementsByClassName("newDiv");
-    while(divsDelete.length > 0){
-        divsDelete[0].parentNode.removeChild(divsDelete[0])
+
+    while(targetDiv.length > 0){
+        targetDiv[0].parentNode.removeChild(targetDiv[0]);
     }
    
-    number = prompt("How many boxes ya want!?")
+    number = prompt("How many boxes ya want!?");
     grid(number);
 }
+
 
